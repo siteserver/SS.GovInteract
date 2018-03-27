8f6b9046164ea6b847c4747162f4a4f17403c4f2
starlying <corp@siteserver.cn>
2018-03-27 13:07:38 +0800

    oracle

---
+++ Core/SqlUtils.cs
@@ -0,0 +1,64 @@
+﻿using SiteServer.Plugin;
+
+namespace SS.GovInteract.Core
+{
+    public static class SqlUtils
+    {
+        public static string EmptyString => Main.Instance.DatabaseType == DatabaseType.Oracle ? "_EMPTY_" : string.Empty;
+
+        public static string GetPageSqlString(string sqlString, string orderString, int itemsPerPage, int currentPageIndex, int pageCount, int recordsInLastPage)
+        {
+            var retval = string.Empty;
+
+            var recsToRetrieve = itemsPerPage;
+            if (currentPageIndex == pageCount - 1)
+            {
+                recsToRetrieve = recordsInLastPage;
+            }
+
+            orderString = orderString.ToUpper();
+            var orderStringReverse = orderString.Replace(" DESC", " DESC2");
+            orderStringReverse = orderStringReverse.Replace(" ASC", " DESC");
+            orderStringReverse = orderStringReverse.Replace(" DESC2", " ASC");
+
+            if (Main.Instance.DatabaseType == DatabaseType.MySql)
+            {
+                retval = $@"
+SELECT * FROM (
+    SELECT * FROM (
+        SELECT * FROM ({sqlString}) AS t0 {orderString} LIMIT {itemsPerPage * (currentPageIndex + 1)}
+    ) AS t1 {orderStringReverse} LIMIT {recsToRetrieve}
+) AS t2 {orderString}";
+            }
+            else if (Main.Instance.DatabaseType == DatabaseType.SqlServer)
+            {
+                retval = $@"
+SELECT * FROM (
+    SELECT TOP {recsToRetrieve} * FROM (
+        SELECT TOP {itemsPerPage * (currentPageIndex + 1)} * FROM ({sqlString}) AS t0 {orderString}
+    ) AS t1 {orderStringReverse}
+) AS t2 {orderString}";
+            }
+            else if (Main.Instance.DatabaseType == DatabaseType.PostgreSql)
+            {
+                retval = $@"
+SELECT * FROM (
+    SELECT * FROM (
+        SELECT * FROM ({sqlString}) AS t0 {orderString} LIMIT {itemsPerPage * (currentPageIndex + 1)}
+    ) AS t1 {orderStringReverse} LIMIT {recsToRetrieve}
+) AS t2 {orderString}";
+            }
+            else if (Main.Instance.DatabaseType == DatabaseType.Oracle)
+            {
+                retval = $@"
+SELECT * FROM (
+    SELECT * FROM (
+        SELECT * FROM ({sqlString}) {orderString} FETCH FIRST {itemsPerPage * (currentPageIndex + 1)} ROWS ONLY
+    ) {orderStringReverse} FETCH FIRST {recsToRetrieve} ROWS ONLY
+) {orderString}";
+            }
+
+            return retval;
+        }
+    }
+}
